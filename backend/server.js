914f6843bc137ab8e657e39e7eb9406e2f8e2fd0
Akash <81360490+Akash-co@users.noreply.github.com>
2024-08-01 15:49:49 +0530

    Chat App Backend Code

---
+++ backend/server.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const path = require('path');
+const { chats } = require("./data/data");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+// Load environment variables from .env file
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+// dotenv.config();
+connectDB();
+// Verify that MONGO_URI is being loaded
+console.log('MONGO_URI:', process.env.MONGO_URI); // This should print the MONGO_URI value
+
+// Connect to the database
+
+
+const app = express();
+
+app.use(express.json());
+// Use the cors middleware
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+// Define the port before using it
+const PORT = process.env.PORT || 5000;
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("API is running..");
+});
+
+
+
+// Start the server and log the message correctly
+app.use(notFound);
+app.use(errorHandler);
+
+
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+);
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+    // credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
+});
+
+
+
+
+
